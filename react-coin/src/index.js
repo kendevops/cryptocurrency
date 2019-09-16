7433d222f1e7a4b6d1c65e6b0f420fc27c9f47b1
Benny Katchy <kennethmichael11@gmail.com>
2019-09-16 13:39:08 +0100

    Creation Of CryptoCurrency Coin App

---
+++ react-coin/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Header from './components/common/Header';
+import List from './components/list/List'
+import './index.css';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <List />
+    </div>
+  )
+}
+
+ReactDom.render(<App />, document.getElementById('root'));
